917eb0496e32fc172e63a2cc0dd9b54a7fc5c16f
zahralizadeh <zahra.alizadeh@gmail.com>
2020-07-31 00:40:55 +0930

    skill section added

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
+import SkillsDetails from "../SkillsDetails";
+
 class Skills extends Component {
   render() {
-    return <div>Skills</div>;
+    return (
+      <section id="skills" className="skills">
+        <div className="container">
+          <div className="section-title">
+            <span>My Skills</span>
+            <h2>My Skills</h2>
+            <p>I like to extend my skills. I enjoy to be a life learner!</p>
+          </div>
+          <Row>
+            <Col className="info-box">
+              <Row className="text-center">
+                {SkillsDetails.map((item, i) => (
+                  <Col xs="4" lg="2" className="mb-5" key={i}>
+                    <img
+                      alt={item.name}
+                      // src="../src/style/img/skills/python.png"
+                      src={item.logo}
+                      className="responsive-img"
+                    />
+                    <span>{item.name}</span>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
+          </Row>
+        </div>
+      </section>
+    );
   }
 }
 export default Skills;
---
+++ src/SkillsDetails.js
@@ -0,0 +1,44 @@
+import python from "./style/img/skills/python.png";
+import django from "./style/img/skills/django.png";
+import drf from "./style/img/skills/drf.png";
+import scrapy from "./style/img/skills/scrapy.png";
+import html5 from "./style/img/skills/html5.png";
+import css3 from "./style/img/skills/css3.png";
+import javascript from "./style/img/skills/javascript.png";
+import bootstrap from "./style/img/skills/bootstrap.png";
+import jquery from "./style/img/skills/jquery.png";
+import react from "./style/img/skills/react.png";
+import mysql from "./style/img/skills/mysql.png";
+import sqlite from "./style/img/skills/sqlite.png";
+import access from "./style/img/skills/access.png";
+import mongodb from "./style/img/skills/mongodb.png";
+import postgresql from "./style/img/skills/postgresql.png";
+import wordpress from "./style/img/skills/wordpress.png";
+import git from "./style/img/skills/git.png";
+import photoshop from "./style/img/skills/photoshop.png";
+
+const SkillsDetails = [
+  { name: "python", logo: python },
+  { name: "django", logo: django },
+  { name: "django REST Framework", logo: drf },
+  { name: "Scrapy", logo: scrapy },
+  { name: "HTML5", logo: html5 },
+  { name: "CSS3", logo: css3 },
+  { name: "Bootstrap", logo: bootstrap },
+  //   { name: "Sass", logo: "./style/img/skills/sass.png" },
+  { name: "Javascript", logo: javascript },
+  //   { name: "JSX", logo: "./style/img/skills/jsx.png" },
+  { name: "jQuery", logo: jquery },
+  { name: "React", logo: react },
+  //   { name: "Webpack", logo: "./style/img/skills/webpack.png" },
+  { name: "MySQL", logo: mysql },
+  { name: "SQLite", logo: sqlite },
+  { name: "mongoDB", logo: mongodb },
+  { name: "PostgreSQL", logo: postgresql },
+  { name: "Access", logo: access },
+  { name: "WordPress", logo: wordpress },
+  { name: "Photoshop", logo: photoshop },
+  { name: "git", logo: git },
+  //   { name: "Heroku", logo: "./style/img/skills/heroku.png" },
+];
+export default SkillsDetails;
